0ea81fb1b699e1d00b61d28fac7ffcc252f067ec
QueeezZ <uince.games@gmail.com>
2022-10-06 21:13:31 +0300

    delete login verification using sms

--- src/sms-verification/interfaces/ISms.service.ts
+++
@@ -1,3 +0,0 @@
-export interface ISmsService {
-	sendMessage(text: string, phone: string): Promise<any>;
-}
--- src/sms-verification/sms.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from "@nestjs/common";
-import { SmsServiceProvider } from "./sms.service";
-
-@Module({
-	providers: [SmsServiceProvider],
-	exports: [SmsServiceProvider],
-})
-export class SmsModule {}
